6e8924d1551db45faf0eec417a6b8d7937af4c14
Kraken <warunamadushanka456@gmail.com>
2025-07-08 08:42:43 +0530

    chore: update development configuration and dependencies
    - Update VS Code settings
    - Configure ESLint with new rules
    - Update Jest configuration and setup
    - Add environment-specific nodemon configurations
    - Update project dependencies

--- jest.config.js
+++ jest.config.js
@@ -17,5 +17,8 @@ export default {
   },
   testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  transformIgnorePatterns: ['<rootDir>/node_modules/(?!(\\.mjs|logger.js))$']
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!(\\.mjs|logger.js))$'],
+  forceExit: true,
+  detectOpenHandles: true,
+  testTimeout: 10000
 };
